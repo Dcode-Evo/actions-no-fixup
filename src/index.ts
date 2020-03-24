a1bfd1fc35798f3154866648ee9c3dbcc8abe2c4
alek <alekart@users.noreply.github.com>
2020-03-24 20:55:22 +0100

    Check pull request "fixup" and "wip" commits and "wip" title

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import {execSync} from 'child_process';
+import * as core from '@actions/core';
+import {context} from '@actions/github';
+
+async function run() {
+  try {
+    const pr = context.payload.pull_request || {base: {ref: ''}, head: {ref: ''}, title: ''};
+    const base = pr.base.ref;
+    const head = pr.head.ref;
+
+    if (/^\[?wip]?(?!\w)/gi.test(pr.title)) {
+      core.setFailed(`Pull request's title indicates WIP status`);
+    }
+
+    const diffCommits = execSync(
+      `git fetch -q && git log --oneline --no-merges --no-decorate origin/${base}...origin/${head}`,
+      {encoding: 'utf8'}
+    ).replace(/^.{8}/gm, '');
+
+    if (/^-{0,2}(fixup!?|wip)(?!\w)/gim.test(diffCommits)) {
+      core.setFailed(`Branch '${head}' contains "fixup" or "WIP" commit(s)`);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
